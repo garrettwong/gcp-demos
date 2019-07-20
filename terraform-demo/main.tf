bd1fff826517185c9b3b06900d9707482400d529
garrettwong <garrettwong@google.com>
2019-07-20 13:15:54 -0700

    add comment delimiters

--- terraform-demo/main.tf
+++ terraform-demo/main.tf
@@ -15,12 +15,15 @@ resource "random_id" "bucket_suffix" {
   byte_length = 6
 }
 
+/* iam_restrict_role: no roles/owner */
 resource "google_project_iam_member" "project" {
   project = "${local.project_id}"
   role    = "roles/owner"
   member  = "user:garrettwong@example.com"
 }
+/* ./ iam_restrict_role: no roles/owner */
 
+/* storage_location: buckets only allowed in asia-southeast1 */
 resource "google_storage_bucket" "image-store" {
   name     = "${format("%s-%s", "demo-bucket", element(random_id.bucket_suffix.*.hex, count.index))}"
   location = "${local.location}"
@@ -30,7 +33,8 @@ resource "google_storage_bucket" "image-store" {
     not_found_page   = "404.html"
   }
 }
-
 output "google_storage_bucket_image-store" {
   value = "${google_storage_bucket.image-store.name}"
 }
+
+/* ./ storage_location: buckets only allowed in asia-southeast1 */
